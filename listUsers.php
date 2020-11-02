a43b23074ae3d013ecf80b640d7377e9e366fc17
pmsdOliveira <46815374+pmsdOliveira@users.noreply.github.com>
2020-11-02 12:48:43 +0000

    Improved user list

--- listUsers.php
+++ listUsers.php
@@ -3,49 +3,42 @@
     <link rel="stylesheet" href="styles.css" />
   </head>
   <body>
-    <table class="list-container" border="1">
-      <thead>
-        <tr class="list-titles">
-          <td>Id</td>
-          <td>Nome</td>
-          <td>Data</td>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-          $pageNum = $_GET['pageNum'];
-          $pageSize = $_GET['pageSize'];
-          $maxIDLastPage = $pageSize * ($pageNum - 1);
-
-          $link = mysqli_connect('localhost', 'root', '', 'SIM')
-            or die('Error connecting to the server: ' . mysqli_error($connect));
-
-          $query = 'SELECT * FROM USERS';
-          $result = mysqli_query($link, $query) or die('The query failed: ' . mysqli_error($link));
-          $totalUsers = mysqli_num_rows($result);
-          $pages = intval($totalUsers / $pageSize + 1);
-          
-          $query = 'SELECT * FROM USERS WHERE ID > ' . $maxIDLastPage . ' LIMIT ' . $pageSize;
-          $result = mysqli_query($link, $query) or die('The query failed: ' . mysqli_error($link));
-          $usersInPage = mysqli_num_rows($result);
-
-          if ($pageSize > $usersInPage)
-            $nRows = $usersInPage;
-          else
-            $nRows = $pageSize;
-
-          for ($i = 1; $i <= $nRows; $i++) {
-            $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
-            echo '<tr>
-                    <td class="list-id">' . $user["ID"] . '</td>
-                    <td class="list-content">' . $user["NAME"] . '</td>
-                    <td class="list-content">' . $user["CREATION_DATE"] . '</td>
-                  </tr>';
-          }
-        ?>
-      </tbody>
-    </table>
     <?php
+      $pageNum = $_GET['pageNum'];
+      $pageSize = $_GET['pageSize'];
+      $maxIDLastPage = $pageSize * ($pageNum - 1);
+
+      $link = mysqli_connect('localhost', 'root', '', 'SIM')
+        or die('Error connecting to the server: ' . mysqli_error($connect));
+
+      $query = 'SELECT * FROM USERS';
+      $result = mysqli_query($link, $query) or die('The query failed: ' . mysqli_error($link));
+      $totalUsers = mysqli_num_rows($result);
+      
+      if ($totalUsers > 0) {
+        echo '<table class="list-container" border="1">
+                <tr class="list-titles">
+                <td>Id</td>
+                <td>Nome</td>
+                <td>Data</td>
+              </tr>';
+
+        $pages = intval($totalUsers / $pageSize + 1);
+      
+        $query = 'SELECT * FROM USERS WHERE ID > ' . $maxIDLastPage . ' LIMIT ' . $pageSize;
+        $result = mysqli_query($link, $query) or die('The query failed: ' . mysqli_error($link));
+        $usersInPage = mysqli_num_rows($result);
+
+        while ($user = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+          echo '<tr>
+                  <td class="list-id">' . $user["ID"] . '</td>
+                  <td class="list-content">' . $user["NAME"] . '</td>
+                  <td class="list-content">' . $user["CREATION_DATE"] . '</td>
+                </tr>';
+        }
+
+        echo '</table>';
+        
         for ($page = 1; $page <= $pages; $page++) {
           $lowerLim = ($page - 1) * $pageSize + 1;
           $maxIDNextPage = $page * $pageSize;
@@ -62,6 +55,9 @@
               . $lowerLim . '-' . $upperLim . '</a>&nbsp;';
           }
         }
+      } else {
+        echo '<p>Ainda não foram registados utilizadores na plataforma.</p>';
+      }
     ?> 
   </body>
 </html>
